cf92f8aaa1d774e11d9dc5860a0ac6e349ecd8a7
PabloBecerraDev <pabloestebanbecerra@gmail.com>
2025-02-07 12:09:36 -0500

    feat: implementacion del formulario register

--- e-comerce_Frontend/src/routes/AppRoutes.tsx
+++ e-comerce_Frontend/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "@/index/homePage"
 import LoginForm from "@/login/loginForm"
+import CreateUserForm from "@/login/createUserForm";
 //import Dashboard from "@/pages/Dashboard"; // futura ruta protegida
 //import ProtectedRoute from "@/components/ProtectedRoute"; // Para proteger rutas más adelante
 
@@ -12,6 +13,7 @@ const AppRoutes = () => {
         {/* Rutas públicas */}
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginForm/>} />
+        <Route path="/register" element={<CreateUserForm/>} />
         
       </Routes>
     </BrowserRouter>
